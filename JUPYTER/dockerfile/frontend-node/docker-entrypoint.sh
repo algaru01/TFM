fdca9db0dac0b4e5c6e71a76e4a9169fe9ee520f
alejandro <alex3galvez@gmail.com>
2024-06-04 14:08:45 +0200

    Improved readability

--- JUPYTER/dockerfile/frontend-node/docker-entrypoint.sh
+++ JUPYTER/dockerfile/frontend-node/docker-entrypoint.sh
@@ -4,8 +4,4 @@ echo "---> Starting the Jupyter Notebook  ..."
     nohup jupyter notebook --ip=0.0.0.0 --port=8888 > nohup_jupyter.out &
 
 echo "---> Starting the Ipython Controller  ..."
-    nohup ipcontroller --reuse > nohup_ipcontroller.out &
-
-while true; do
-    sleep 3600  # Espera 1 hora antes de volver a verificar
-done
+    ipcontroller --reuse 
--- JUPYTER/dockerfile/worker-node/docker-entrypoint.sh
+++ JUPYTER/dockerfile/worker-node/docker-entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 echo "---> Starting the Ipython Engine  ..."
-    nohup ipengine > nohup_ipcontroller.out &
-
-while true; do
-    sleep 3600  # Espera 1 hora antes de volver a verificar
-done
+    ipengine 
