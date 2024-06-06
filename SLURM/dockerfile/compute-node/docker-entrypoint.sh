16935d31bbf28eb2681f1e4e598dc6838ac13c78
alejandro <alex3galvez@gmail.com>
2024-06-06 17:23:56 +0200

    First migration towards k8s

--- SLURM/dockerfile/compute-node/docker-entrypoint.sh
+++ SLURM/dockerfile/compute-node/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#echo "172.17.0.2    linux0" >> /etc/hosts
+#echo "172.17.0.2    linux-hn" >> /etc/hosts
 
 echo "---> Starting the Secure Shell daemon (sshd)  ..."
     /usr/sbin/sshd
@@ -8,13 +8,13 @@ echo "---> Starting the Secure Shell daemon (sshd)  ..."
 echo "---> Starting the MUNGE Authentication service (munged) ..."
     sudo -u munge /etc/init.d/munge start
 
-echo "---> Waiting for head node (linux0) to become active before starting slurmd..."
-    until 2>/dev/null >/dev/tcp/linux0/6817
+echo "---> Waiting for head node (linux-hn) to become active before starting slurmd..."
+    until 2>/dev/null >/dev/tcp/linux-hn/6817
     do
-        echo "-- head node (linux0) is not available.  Sleeping ..."
+        echo "-- head node (linux-hn) is not available.  Sleeping ..."
         sleep 2
     done
-echo "-- head node (linux0) is now active ..."
+echo "-- head node (linux-hn) is now active ..."
 
 echo "---> Starting the Slurm Node Daemon (slurmd) ..."
     exec /usr/sbin/slurmd -Dvvv
